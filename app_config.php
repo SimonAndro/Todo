1bbee2a9918c462b9681d67a4575cdf650a3bec8
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-04-24 12:14:53 +0800

    app config
    update .gitignore

---
+++ app_config.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+
+    /**
+     * Database connection
+     */
+    'db_host' => 'localhost',
+
+    'db_username' => 'root',
+
+    'db_name' => 'todo_task_manager_db',
+
+    'db_password' => '',
+
+    /**
+     * Site information
+     */
+    'site-title'=>'Todo',
+    'base-url'=>'http://localhost/todo/' 
+);
