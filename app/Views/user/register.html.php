c8070cb71e239b58cfc47c7568b095c842ea17b1
SimonAndro <41785978+SimonAndro@users.noreply.github.com>
2021-05-02 14:20:44 +0800

    unmerge

--- app/Views/user/register.html.php
+++ app/Views/user/register.html.php
@@ -8,35 +8,35 @@
 </head>
 
 <body>
-    <div class="register-layout">
-        <form action="user-register" method="post" >
-            <div class="container">
-                <h1>Register</h1>
-                <?php if(isset($error)): foreach($error as $e):?>
-                <div class="reg-error"><p><?=$e?></p></div>
-                <?php endforeach; endif; ?>
-                <p>Please fill in this form to create an account.</p>
-                <hr>
 
-                <label for="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="val[email]" id="email" required>
+    <form action="user-register" method="post" >
+        <div class="container">
+            <h1>Register</h1>
+            <?php if(isset($error)): foreach($error as $e):?>
+            <div class="reg-error"><p><?=$e?></p></div>
+            <?php endforeach; endif; ?>
+            <p>Please fill in this form to create an account.</p>
+            <hr>
 
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="val[password]" id="psw" required>
+            <label for="email"><b>Email</b></label>
+            <input type="text" placeholder="Enter Email" name="val[email]" id="email" required>
 
-                <label for="psw-repeat"><b>Repeat Password</b></label>
-                <input type="password" placeholder="Repeat Password" name="val[passwordRepeat]" id="psw-repeat" required>
-                <hr>
-                <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
+            <label for="psw"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="val[password]" id="psw" required>
 
-                <button type="submit" class="registerbtn">Register</button>
-            </div>
+            <label for="psw-repeat"><b>Repeat Password</b></label>
+            <input type="password" placeholder="Repeat Password" name="val[passwordRepeat]" id="psw-repeat" required>
+            <hr>
+            <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
 
-            <div class="container signin">
-                <p>Already have an account? <a href="user-login">Sign in</a>.</p>
-            </div>
-        </form>
-    </div> <!--Register Layout -->
+            <button type="submit" class="registerbtn">Register</button>
+        </div>
+
+        <div class="container signin">
+            <p>Already have an account? <a href="user-login">Sign in</a>.</p>
+        </div>
+    </form>
+
 </body>
 
 </html>
